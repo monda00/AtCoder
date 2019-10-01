23950ddfe6097df6f33729760f385bd987247a9d
monda <monda0524@gmail.com>
2019-10-02 07:54:01 +0900

    add B

---
+++ ABC003/B.py
@@ -0,0 +1,21 @@
+def is_atcoder(c):
+    if c == 'a' or 't' or 'c' or 'o' or 'd' or 'e' or 'r':
+        return True
+    else:
+        return False
+
+s1 = input()
+s2 = input()
+
+for c1, c2 in zip(s1, s2):
+    if c1 == '@' and is_atcoder(c2):
+        continue
+    elif c2 == '@' and is_atcoder(c1):
+        continue
+    elif c1 == c2:
+        continue
+    else:
+        print('You will lose')
+        exit()
+print('You can win')
+
