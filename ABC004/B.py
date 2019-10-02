9fe17e65839f87832a10ace9aae4d271ecc7ba5e
monda00 <monda0524@gmail.com>
2019-10-02 13:39:56 +0900

    add B

---
+++ ABC004/B.py
@@ -0,0 +1,10 @@
+c1 = list(input().split())
+c2 = list(input().split())
+c3 = list(input().split())
+c4 = list(input().split())
+
+print(*c4[::-1])
+print(*c3[::-1])
+print(*c2[::-1])
+print(*c1[::-1])
+
