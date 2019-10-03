8ba606543e7ebf0a3febb6eb2afb43300d928316
monda00 <monda0524@gmail.com>
2019-10-03 15:01:22 +0900

    edit B

--- ABC006/B.py
+++ ABC006/B.py
@@ -1,9 +1,18 @@
 n = int(input())
-a = [0] * n
-a[0] = 0
-a[1] = 0
-a[2] = 1
-for i in range(3, n):
-    a[i] = a[i-1] + a[i-2] + a[i-3]
-print(a[n-1] % 10007)
+a_0 = 0
+a_1 = 0
+a_2 = 1
+if n == 1:
+    print(a_0)
+elif n == 2:
+    print(a_1)
+elif n == 3:
+    print(a_2)
+else:
+    for i in range(3, n):
+        a = a_0 + a_1 + a_2
+        a_0 = a_1
+        a_1 = a_2
+        a_2 = a
+    print(a % 10007)
 
