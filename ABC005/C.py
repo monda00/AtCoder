f2c5e1637f6e1e5bd3a6317d83f9bc0ae5c56815
monda <monda0524@gmail.com>
2019-11-07 21:50:04 +0900

    add C

--- ABC005/C.py
+++ ABC005/C.py
@@ -1,3 +1,7 @@
+def exist_takoyaki(customer_time, takoyaki_time):
+    return takoyaki_time != -1 and takoyaki_time + allowable_time >= customer_time \
+        and customer_time >= takoyaki_time
+
 allowable_time = int(input())
 takoyaki_num = int(input())
 takoyaki_time_li = list(map(int, input().split()))
@@ -5,6 +9,14 @@ customer_num = int(input())
 customer_time_li = list(map(int, input().split()))
 
 # 客がきたときに最小時間で提供可能なたこ焼きを探す
+# 使ったたこ焼きは-1にする
 for customer_time in customer_time_li:
-    for tokoyaki_time in takoyaki_time_li:
+    for i, takoyaki_time in enumerate(takoyaki_time_li):
+        if exist_takoyaki(customer_time, takoyaki_time):
+            takoyaki_time_li[i] = -1
+            break
+        if len(takoyaki_time_li) == i + 1:
+            print('no')
+            exit()
+print('yes')
 
