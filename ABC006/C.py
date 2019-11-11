174a92e191d67f7f745b865dccbb8575762e27c0
monda00 <monda0524@gmail.com>
2019-11-11 18:14:44 +0900

    edit C

--- ABC006/C.py
+++ ABC006/C.py
@@ -5,14 +5,15 @@ adult_leg = 2
 
 for aged_num in range(pepole_num + 1):
     # すでにlegs_nmを超えていたら
-    if  aged_num * aged_leg > legs_num:
+    if aged_num * aged_leg > legs_num:
         break
     # 残りが奇数だと4と2では構成できない
     if (legs_num - aged_num * aged_leg) % 2 == 1:
         continue
     for baby_num in range(pepole_num - aged_num + 1):
         adult_num = pepole_num - baby_num - aged_num
-        sum_legs = baby_num * baby_leg + aged_num * aged_leg + adult_num * adult_leg
+        sum_legs = baby_num * baby_leg + aged_num * aged_leg + \
+            adult_num * adult_leg
         # すでにlegs_nmを超えていたら
         if sum_legs > legs_num:
             break
