58cf95f650f643a21b3cf41f2c917a63ce48b9a8
monda <monda0524@gmail.com>
2019-12-01 10:25:27 +0900

    add C

--- ABC008/C.py
+++ ABC008/C.py
@@ -1,6 +1,20 @@
+import math
+
 coin_num = int(input())
 coin_li = list()
 for _ in range(coin_num):
     coin_li.append(int(input()))
 
+ans = 0
+for i in range(len(coin_li)):
+    divisor_sum = 0
+    for j in range(len(coin_li)):
+        if i != j and coin_li[i] % coin_li[j] == 0:
+            divisor_sum += 1
+    if divisor_sum % 2 == 1:
+        ans += 1/2.0
+    else:
+        ans += (divisor_sum + 2.0) / (2 * divisor_sum + 2.0)
+
+print(ans)
 
