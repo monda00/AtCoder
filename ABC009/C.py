3570978e45a4adae9c69f4beba76c9f0409d8ba1
monda <monda0524@gmail.com>
2019-12-11 07:55:12 +0900

    udpate C

--- ABC009/C.py
+++ ABC009/C.py
@@ -1,15 +1,26 @@
 char_num, changed_limit = map(int, input().split())
 input_string = list(input())
-original_string = input_string
-changed_string = list()
+changed_num = 0
 
 for i, c in enumerate(input_string):
-    if i < changed_limit:
-        changed_string += min(original_string)
-        original_string.remove(min(original_string))
+    if changed_num < changed_limit and input_string[i] != min(input_string[i:]):
+        print(input_string.index(min(input_string[i:])))
+        print(i)
+        print(input_string)
+        tmp = input_string[i]
+        print('tmp = {}'.format(tmp))
+        print('input_string[{}] = {}'.format(i, input_string[i]))
+        input_string[i] = input_string[input_string.index(min(input_string[i:]))]
+        min_index = input_string.index(min(input_string[i:]))
+        print('input_string[{}] = {}'.format(i, input_string[i]))
+        print('input_string[{}] = {}'.format(input_string.index(min(input_string[i:])), input_string[input_string.index(min(input_string[i:]))]))
+        print('min_index = {}'.format(min_index))
+        input_string[min_index] = tmp
+        print('input_string[{}] = {}'.format(input_string.index(min(input_string[i:])), input_string[input_string.index(min(input_string[i:]))]))
+        print(input_string.index(min(input_string[i:])))
+        print(i)
+        print(input_string)
+        changed_num += 2
 
-print(original_string)
-print(changed_string)
-result = changed_string + original_string
-print(''.join(result))
+print(''.join(input_string))
 
