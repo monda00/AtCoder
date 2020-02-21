2ec6570061556467c8c13985237c5c5065f22303
monda <monda0524@gmail.com>
2020-02-22 00:45:49 +0900

    update D

--- ABC155/D.py
+++ ABC155/D.py
@@ -12,29 +12,30 @@ l = -10**18
 r = 10**18
 
 # 二分探索
-# c以下になる組み合わせがいくつあるか計算する
+# x以下になる組み合わせがいくつあるか計算する
 while l+1<r:
-    c = (l + r) // 2
-    print('c :', c)
+    x = (l + r) // 2
+    print('x :', x)
     cnt = 0
 
     # 0になる組み合わせの個数のカウントを追加
-    if c >= 0:
+    if x >= 0:
         cnt += n * len(zero)
 
-    print(c//pos)
-    print('c//pos: ', np.searchsorted(a, c//pos, side='right').sum())
-    print('(-c-1)//(-neg): ',
-          (n-np.searchsorted(a, (-c-1)//(-neg), side='right')).sum())
-    cnt += np.searchsorted(a,c//pos,side='right').sum() + (n-np.searchsorted(a,(-c-1)//(-neg),side='right')).sum()
-    cnt -= np.count_nonzero(a*a<=c)
+    print('x//pos: ', x//pos)
+    print('pos: ', a.searchsorted(x//pos,side='right').sum())
+    print('-(-x//neg): ', -(-x//neg))
+    print('neg: ', (n-a.searchsorted(-(-x//neg),side='right')).sum())
+    cnt += a.searchsorted(x//pos,side='right').sum()
+    cnt += (n-a.searchsorted(-(-x//neg),side='left')).sum()
+    cnt -= np.count_nonzero(a*a<=x)
     cnt//=2
 
     print('cnt :', cnt)
 
     if cnt >= k:
-        l = c
+        r = x
     else:
-        r = c
+        l = x
 
-print(l)
+print(r)
