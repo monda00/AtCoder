4d4a52396a2f961a8d80b66d7deec0a38a3b890b
monda00 <monda0524@gmail.com>
2021-06-15 21:46:21 +0900

    update abc128

--- AtCoderProblem/Difficulty800/abc128_c_805.py
+++ AtCoderProblem/Difficulty800/abc128_c_805.py
@@ -9,22 +9,26 @@ for _ in range(m):
     tmp = list(map(int, input().split()))
     k.append(tmp[0])
     s.append(tmp[1:])
-
 p = list(map(int, input().split()))
 
 ans = 0
 for i in range(2**n):
     switch = [0] * n
+    # スイッチのon/offのパターンをビット探索で
     for j in range(n):
         if(i >> j) & 1:
             switch[j] = 1
+
+    # 全ての電球が点灯するか
+    check = True
     for j in range(m):
         k_sum = 0
         for s_i in range(k[j]):
             k_sum += switch[s[j][s_i]-1]
         if k_sum % 2 != p[j]:
+            check = False
             break
-        if j == m-1:
-            ans += 1
+    if check:
+        ans += 1
 
 print(ans)
