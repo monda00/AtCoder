172aaf4852f707e5a2d79879f00feb9a6124b7d0
monda00 <monda0524@gmail.com>
2021-07-08 00:01:06 +0900

    update dp_frog1

--- Sample/dp_frog1.py
+++ Sample/dp_frog1.py
@@ -4,7 +4,7 @@ h = list(map(int, input().split()))
 c = [10**5+1] * n
 
 c[0] = 0
-c[1] = abs(H[1]-H[0])
+c[1] = abs(h[1]-h[0])
 
 for i in range(2, n):
     c[i] = min(abs(h[i]-h[i-1])+c[i-1], abs(h[i]-h[i-2])+c[i-2])
